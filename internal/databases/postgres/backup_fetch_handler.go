50623851cc4b40abd76f8dc67542c8c57abfa8f1
Ben Banfield-Zanin <benbz@matrix.org>
2025-09-03 09:50:30 +0100

    Handle non-incremental

--- internal/databases/postgres/backup_fetch_handler.go
+++ internal/databases/postgres/backup_fetch_handler.go
@@ -90,6 +90,9 @@ func deltaFetchRecursionOld(backup Backup, folder storage.Folder, dbDataDirector
 
 		return backup.unwrapToEmptyDirectory(dbDataDirectory, filesToUnwrap, false, extractProv)
 	}
+
+	tracelog.InfoLogger.Printf("Not unwrapping full base-backup, %v is here", dbDataDirectory)
+	return nil
 }
 
 func GetPgFetcherOld(dbDataDirectory, fileMask, restoreSpecPath string,
